72124771d8ff4fb03ffd8ce411d2b945efd9f027
jjunno <joonas.junno@protonmail.com>
2024-07-14 13:33:42 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pyrrs
- 
+
+Python RRS reader
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ src/main.py
@@ -0,0 +1,10 @@
+def main():
+  print("Hello, world!")
+  return None
+
+if __name__ == "__main__":
+  main()
+  # return None
+  # while True:
+  #   main()
+  #   time.sleep(MAIN_INTERVAL_SECONDS)
