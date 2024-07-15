1803baeaea2a74d5b53c9fd0831a3e1f046324e8
jjunno <joonas.junno@protonmail.com>
2024-07-15 12:24:06 +0300

    added express

--- nodejs_inner_rest/server.js
+++ nodejs_inner_rest/server.js
@@ -1,3 +1,23 @@
 import express from 'express';
 
 import routes from './routes.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = 3000;
+
+const app = express();
+
+app.use('/innerapi', routes);
+
+/**
+ * Catch all route.
+ */
+app.all('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found.' });
+});
+
+app.listen(PORT, () => {
+  console.info(`Inner NodeJS Express is running on port ${PORT}`);
+});
--- nodejs_inner_rest/service.js
+++ nodejs_inner_rest/service.js
@@ -1,7 +1,7 @@
 import knex from './knex.js';
 
-export async function create() {
-  const { name, value } = req.body;
-  await knex('settings').insert({ origin_id: 123 });
+export async function create(req, res) {
+  const t = req.body;
+  await knex('articles').insert({ origin_id: 123 });
   res.status(201).send();
 }
